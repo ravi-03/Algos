00a0cc5133a22d5956471e0d6310ea9ac7e89e58
Ravi Shankar Raj <ravi@Ravis-MacBook-Air.local>
2025-05-12 22:07:35 +0530

    efficient nCr mod P

--- Maths/nCrMODp/nCrMODp.cpp
+++ Maths/nCrMODp/nCrMODp.cpp
@@ -6,6 +6,47 @@ using namespace std;
 
 const int P = 1e9 + 7;
 
+int power(int x, int y) {
+	if (y == 0) {
+		return 1;
+	}
+	if (x == 0) {
+		return 0;
+	}
+	assert(y > 0);
+	int ans = 1;
+	while (y) {
+		if (y & 1) {
+			ans *= x;
+			ans %= P;
+		}
+		x = x * x % P;
+		y = y >> 1;
+	}
+	return ans;
+	//TC: O(log2(y))
+}
+
+int nCrMODp2(int n, int r) {
+	if (r > n - r) {
+		r = n - r;
+	}
+	//nCr = (n * n - 1 * .... * n - r + 1) / fact(r)
+	int num = 1;
+	for (int i = n; i >= n - r + 1; i--) {
+		num *= i;
+		num %= P;
+	}
+	int den = 1;
+	for (int i = r; i >= 1; i--) {
+		den *= i;
+		den %= P;
+	}
+	//using fermat's little theorem
+	return num * power(den, P - 2) % P;
+	//TC: O(n)
+}
+
 int nCrMODp(int n, int r) {
 	if(r > n - r) 
 		r = n - r;
@@ -26,6 +67,7 @@ int nCrMODp(int n, int r) {
 void runTests() {
 	int n, r; cin >> n >> r;
 	cout << nCrMODp(n, r) << '\n';
+	cout << nCrMODp2(n, r) << '\n';
 }
 
 int32_t main() {
